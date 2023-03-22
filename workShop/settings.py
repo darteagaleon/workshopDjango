236db72297b5bc84fd5c7ff9afcda11e1b59d7be
Diego Fernando Arteaga Leon <darteagaleon@gmail.com>
2023-03-22 15:55:08 -0500

    Actualizacion 1.1

--- workShop/settings.py
+++ workShop/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appProductos',
+    'appUsuarios',
 ]
 
 MIDDLEWARE = [
@@ -68,7 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'workShop.wsgi.application'
-
+AUTH_USER_MODEL = 'appUsuarios.Usuario'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
@@ -115,8 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'workshopDjango/static'
-
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIR=[
+    'workshop/static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- workShop/views.py
+++ workShop/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def home(request):
     return render(request,'home.html')
 
+def base(request):
+    return render(request,'base.html')
